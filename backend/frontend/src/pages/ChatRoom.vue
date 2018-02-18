50f2d3a78a5fee8d10bcc4036cbfce17b9949112
sungkukkim <sungkuk5420@gmail.com>
2018-02-18 12:57:17 +0900

    채팅입력폼 넣어서 레이아웃 재배치

--- backend/frontend/src/pages/ChatRoom.vue
+++ backend/frontend/src/pages/ChatRoom.vue
@@ -1,33 +1,34 @@
 <template>
-  <div class="layout-padding" style="max-width: 500px; padding-top:60px;">
-    <q-layout>
-      <!-- your content -->
-      <q-fixed-position corner="top" :offset="[0, 0]">
-        <back-btn-toolbar></back-btn-toolbar>
-      </q-fixed-position>
-      <div >
-        <q-chat-message
-          v-for="msg in chatMessages"
-          :key="msg.key"
-          :label="msg.label"
-          :sent="(msg.name === userInfo.name) ? true : false"
-          :text-color="'black'"
-          :bg-color="''"
-          :name="msg.name"
-          :avatar="(msg.name === userInfo.name) ? 'http://quasar-framework.org/quasar-play/android/img/boy-avatar.5ff53af.png' : 'http://quasar-framework.org/quasar-play/android/statics/linux-avatar.png'"
-          :text="msg.text"
-          :stamp="msg.stamp"
-        />
+  <q-layout>
+    <!-- your content -->
+    <q-toolbar slot="header" class="fixed-top no-padding">
+      <back-btn-toolbar></back-btn-toolbar>
+    </q-toolbar>
+    <div class="layout-padding" style="padding-top: 60px; padding-bottom: 60px;">
+      <q-chat-message
+        v-for="msg in chatMessages"
+        :key="msg.key"
+        :label="msg.label"
+        :sent="(msg.name === userInfo.name) ? true : false"
+        :text-color="'black'"
+        :bg-color="''"
+        :name="msg.name"
+        :avatar="(msg.name === userInfo.name) ? 'http://quasar-framework.org/quasar-play/android/img/boy-avatar.5ff53af.png' : 'http://quasar-framework.org/quasar-play/android/statics/linux-avatar.png'"
+        :text="msg.text"
+        :stamp="msg.stamp"
+      />
 
-        <q-chat-message
-          name="Vladimir"
-          avatar="http://quasar-framework.org/quasar-play/android/img/boy-avatar.5ff53af.png"
-        >
-          <q-spinner-dots size="2rem" />
-        </q-chat-message>
-      </div>
-    </q-layout>
-  </div>
+      <q-chat-message
+        name="Vladimir"
+        avatar="http://quasar-framework.org/quasar-play/android/img/boy-avatar.5ff53af.png"
+      >
+        <q-spinner-dots size="2rem" />
+      </q-chat-message>
+    </div>
+    <q-toolbar slot="footer" class="fixed-bottom no-margin no-padding">
+        <q-input :loading="loading" class="no-margin full-width" inverted color="secondary" v-model="text" stack-label="send message" />
+    </q-toolbar>
+  </q-layout>
 </template>
 
 <script>
@@ -42,7 +43,8 @@ import {
   QBtn,
   QIcon,
   QLayout,
-  QFixedPosition
+  QFixedPosition,
+  QInput
 } from 'quasar'
 
 export default {
@@ -55,10 +57,11 @@ export default {
     QLayout,
     QFixedPosition,
     backBtnToolbar,
-    qChatMessage
+    qChatMessage,
+    QInput
   },
   data () {
-    return {}
+    return {loading: true}
   },
   computed: {
     ...mapGetters({
